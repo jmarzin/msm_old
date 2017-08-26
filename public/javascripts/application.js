fe814ed61003703ae33c33b9fd0c9e8496973bac
jmarzin <jacques.marzin@free.fr>
2017-08-26 11:55:48 +0200

    samedi

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,6 +14,10 @@ function editeur() {
             'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
             'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
             'save table contextmenu directionality emoticons template paste textcolor'
-        ]
+        ],
+        file_picker_callback: function(callback, value, meta) {
+            if(meta.filetype == 'image')
+                $('#picker').trigger( "click" );
+        }
     });
 }
